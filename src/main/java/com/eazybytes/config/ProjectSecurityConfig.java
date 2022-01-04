d2f5541aa96e315ea68a8adf9c375a9e8bb45ebb
Virjanand Panday <virjanand@virjanand.com>
2022-01-04 09:16:15 +0100

    Define users and authentication in configuration

--- src/main/java/com/eazybytes/config/ProjectSecurityConfig.java
+++ src/main/java/com/eazybytes/config/ProjectSecurityConfig.java
@@ -1,9 +1,11 @@
 package com.eazybytes.config;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.annotation.web.configurers.ExpressionUrlAuthorizationConfigurer;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 
 @Configuration
 public class ProjectSecurityConfig extends WebSecurityConfigurerAdapter {
@@ -20,5 +22,12 @@ public class ProjectSecurityConfig extends WebSecurityConfigurerAdapter {
         });
         http.formLogin();
         http.httpBasic();
+    }
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception{
+        auth.inMemoryAuthentication().withUser("admin").password("12345").authorities("admin").and()
+                .withUser("user").password("12345").authorities("read").and()
+                .passwordEncoder(NoOpPasswordEncoder.getInstance());
     }
 }
